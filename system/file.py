74d663a2b217b5c32e3cacf9faff37e51cd22823
Jyoti Pandey <jyoti@plenty.ag>
2020-08-27 21:24:35 -0700

    change syntax for file opening

--- system/file.py
+++ system/file.py
@@ -1,3 +1,11 @@
 def readFileAsString(file_name):
-    with open(file_name) as f:
-        return f.read()
+    file = None
+    try:
+        file = open(file_name)
+        content = file.read()
+        return content
+    except BaseException, e:
+        print('Error occurred while opening a file', e)
+    finally:
+        if file:
+            file.close()
